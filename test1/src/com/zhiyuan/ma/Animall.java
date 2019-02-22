6a629dce92b7c1d6039d7d0c6feb96b71718d9f0
mzy1234 <mzy_157@163.com>
2019-02-22 09:16:17 +0800

    第三次提交 作者：马志验

--- test1/src/com/zhiyuan/ma/Animall.java
+++ test1/src/com/zhiyuan/ma/Animall.java
@@ -3,5 +3,8 @@ package com.zhiyuan.ma;
 public class Animall {
     public static void main(String[] args) {
         System.out.println("dhghfdjghhd");
+        System.out.println("dhghfdjghhd");
+        System.out.println("dhghfdjghhd");
+        System.out.println("dhghfdjghhd");
     }
 }
