8b57dcf0d781af28b53b96fa6636f252173a2df2
mzy1234 <mzy_157@163.com>
2019-02-22 15:08:57 +0800

    第五次提交 作者：马志验

---
+++ learncenter/src/main/java/com/zhiyuan/ma/learncenter/service/impl/UserServiceImpl2.java
@@ -0,0 +1,13 @@
+package com.zhiyuan.ma.learncenter.service.impl;
+
+import com.zhiyuan.ma.learncenter.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl2 implements UserService {
+
+    @Override
+    public void getUserById() {
+
+    }
+}
