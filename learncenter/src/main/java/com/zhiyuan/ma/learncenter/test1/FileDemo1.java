8e811438b893a9b7abe0dad8d01914b70956190d
mzy1234 <mzy_157@163.com>
2019-02-25 19:11:29 +0800

    开发分支的第二次提交 作者：马志验

---
+++ learncenter/src/main/java/com/zhiyuan/ma/learncenter/test1/FileDemo1.java
@@ -0,0 +1,23 @@
+package com.zhiyuan.ma.learncenter.test1;
+
+
+import java.io.File;
+
+public class FileDemo1 {
+
+    public static void main(String[] args) {
+        File file = new File("D:\\filedemo");
+        getFile(file);
+    }
+    public static void getFile(File file){
+        File[] files = file.listFiles();
+        for (File file1 : files) {
+            if(file1.isFile()){
+                System.out.println("---"+file1.getName());
+            }else if(file1.isDirectory()){
+                System.out.println("---"+file1.getName());
+                getFile(file1);
+            }
+        }
+    }
+}
